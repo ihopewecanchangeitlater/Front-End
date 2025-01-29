184b134a8318cda17351225596fb4f33e7a29f8c
Kostas Thomson <kostasthomson12@gmail.com>
2025-01-29 13:47:18 +0200

    Environment variables simplification

--- src/Utils/Endpoints.js
+++ src/Utils/Endpoints.js
@@ -1,13 +1,12 @@
-export const BASE_URL = process.env.REACT_APP_API_BASE_URL;
-export const AUTH_LOGIN_URL = process.env.REACT_APP_AUTH_LOGIN_URL;
-export const AUTH_REGISTER_URL = process.env.REACT_APP_AUTH_REGISTER_URL;
-export const AUTH_LOGOUT_URL = process.env.REACT_APP_AUTH_LOGOUT_URL;
-export const CARS_URL = process.env.REACT_APP_CARS_URL;
-export const CARS_ADD_URL = process.env.REACT_APP_CARS_ADD_URL;
-export const CARS_SEARCH_URL = process.env.REACT_APP_CARS_SEARCH_URL;
-export const CARS_QUANTITY_URL = process.env.REACT_APP_CARS_QUANTITY_URL;
-export const CARS_BUY_URL = process.env.REACT_APP_CARS_BUY_URL;
-export const RESERVATIONS_URL = process.env.REACT_APP_RESERVATIONS_URL;
-export const RESERVATIONS_USER_URL =
-	process.env.REACT_APP_RESERVATIONS_USER_URL;
-export const RESERVATIONS_CAR_URL = process.env.REACT_APP_RESERVATIONS_CAR_URL;
+export const BASE_URL = `${process.env.REACT_APP_API_BASE_URL}/api`;
+export const AUTH_LOGIN_URL = "/auth/login";
+export const AUTH_REGISTER_URL = "/auth/register";
+export const AUTH_LOGOUT_URL = "/auth/logout";
+export const CARS_URL = "/cars";
+export const CARS_ADD_URL = "/cars/buy";
+export const CARS_SEARCH_URL = "/cars/search";
+export const CARS_QUANTITY_URL = "/cars/updateQuantity";
+export const CARS_BUY_URL = "/cars/addcar";
+export const RESERVATIONS_URL = "/reservations";
+export const RESERVATIONS_USER_URL = "/reservations/user";
+export const RESERVATIONS_CAR_URL = "/reservations/car";
